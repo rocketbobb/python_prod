cea37a199522866ca3924a0b848c7e298eff5444
bbonham-intf <bob_bonham@yahoo.com>
2023-01-17 12:31:15 -0500

    updates

--- lib/set_env.py
+++ lib/set_env.py
@@ -81,23 +81,6 @@ class Init_Env:
     def addGlobal(self, dict):
         return self.globals_dict['globals'].update(dict)
 
-    def keys_exists(self, element, *keys):
-        '''
-        Check if *keys (nested) exists in `element` (dict).
-        '''
-        if not isinstance(element, dict):
-            raise AttributeError('keys_exists() expects dict as first argument.')
-        if len(keys) == 0:
-            raise AttributeError('keys_exists() expects at least two arguments, one given.')
-
-        _element = element
-        for key in keys:
-            try:
-                _element = _element[key]
-            except KeyError:
-                return False
-        return True
-
 if __name__ == '__main__':
     pgmname = os.path.splitext(os.path.basename(sys.argv[0]))[0]
     print(f'\n**** Running main Program [{pgmname}]\n')
